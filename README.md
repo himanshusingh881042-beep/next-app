e31c2acf3998e9d3a9694cfbaeb57e842cb71359
Himanshu Singh <himanshu16.singh@tataaig.com>
2026-05-03 23:44:34 +0530

    removing code

--- README.md
+++
@@ -1,5 +0,0 @@
-Next.js Latest Project
-
-Start: npm run dev
-Build: npm run dev
-Build Start: npm run start
--- src/pages/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Index = () => {
-  return (
-    <div className="container">Bitbucket Demo</div>
-  )
-}
-
-export default Index
